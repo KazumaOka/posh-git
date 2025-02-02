5e8b4af67ec1a5c150524c86bd6e9083c10700c2
aaa <aaa@aaa.com>
2025-02-02 17:29:54 +0900

    修正。

--- src/GitTabExpansion.ps1
+++ src/GitTabExpansion.ps1
@@ -267,8 +267,12 @@ function script:expandGitAlias($cmd, $rest) {
             return "git $known$rest"
         }
 
-        if (($alias -match '^(?<beforeSemicolon>.*?)\s*;') -and ($Matches['beforeSemicolon'] -match '^.*?:\s*(?<afterColon>.*)$')) {
-            return "git $($Matches['afterColon'])$rest"
+        if (
+                 ($alias -match '^\s*\!\s*(?<functionName>\S+)\s*\(\s*\)\s*\{\s*(?<functionBody>.+)\s*\}\s*;\s*(?<functionCall>\S+)\s*$') `
+            -and ($Matches['functionName'] -ceq $Matches['functionCall']) `
+            -and ($Matches['functionBody'] -match ':\s*(git)?\s*(?<nullSubCommand>\S+)\s*;')
+        ) {
+            return "git $($Matches['nullSubCommand'])$rest"
         }
 
         return "git $alias$rest"
